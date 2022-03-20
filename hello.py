0eca1c82d1e7a5a9282d6dc43d7964571a7d7aa4
Jonathan Nanno <jon.nanno@gmail.com>
2022-03-20 17:59:22 -0400

    Created form page, added a key, added new route to new form page

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/') 
@@ -33,3 +42,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    
+    return render_template("name.html", name=name, form=form)
